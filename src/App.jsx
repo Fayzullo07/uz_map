8fd5407e63f85964880c7959d346dc8b19989469
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-08-21 15:48:17 +0500

    tree map

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TreeMap from "./components/TreeMap";
+import UzMap from "./components/UzMap";
+
+function App() {
+  return (
+    <>
+      {/* <UzMap /> */}
+      <TreeMap />
+    </>
+  );
+}
+
+export default App;
